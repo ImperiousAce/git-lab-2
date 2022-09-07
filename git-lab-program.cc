511a2e4b378aea3c928d25441373d2ea8eddf362
ImperiousAce <josephash87@gmail.com>
2022-09-07 17:51:45 -0400

    intial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *        Author: Joe Ash
+ *        Date: 09/07/2022
+ *        Description: Second C++ Program I have ever made 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
